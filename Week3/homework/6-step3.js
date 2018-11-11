15200f262b31f704d477a0c08e0992dc7e3cd7bc
Farhn-Nazir <nazirf3440@gmail.com>
2018-11-11 10:14:14 +0100

    Done

--- Week3/homework/6-step3.js
+++ Week3/homework/6-step3.js
@@ -4,3 +4,5 @@ const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 // add your solution here, or add a comment on how you would tackle this problem
+arr2d.map(e => e.map(v => console.log(v)));
+arr3d.map(e => e.map(v => v.map(j => console.log(j))));
